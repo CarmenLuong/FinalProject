b9c07594a1351e330b17ab9f7eb4f94d3fec9c0a
mpak0002 <mpak0002@algonquinlive.com>
2020-07-29 15:44:40 -0400

    made Main xml and java class for soccer Highlights

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button goToScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToScore = findViewById(R.id.SoccerMatch);
+
+        goToScore.setOnClickListener( Click -> { Intent SeeScores = new Intent (MainActivity.this, SoccerMain.class);
+        startActivity(SeeScores);
+
+        });
+
+
     }
+
+
 }
